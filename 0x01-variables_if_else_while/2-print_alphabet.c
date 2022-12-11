654b989f2a23f13ed594114673a98ab41a5d8f9a
Sarah Okoronkwo <o.sarah.ez@gmail.com>
2022-12-11 10:54:25 -0800

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,10 @@
  * *main - Program to print alphabet letter followed by new line
  * *
  * *Return: return 0
- * */
+ */
 int main(void)
 {
 	int letter;
-			
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		letter = tolower(letter);
